2ca68e3ba5bf3b424b43cd0a7eebce64784da6ad
huoxin233 <23447157+huoxin233@users.noreply.github.com>
2026-03-24 03:53:52 +0800

    移除不再使用的历史监听器和事件

--- extend.php
+++ extend.php
@@ -10,33 +10,10 @@
  */
 
 use Flarum\Extend;
-use Flarum\Post\Event\Posted;
-use Flarum\Post\Event\Restored as PostRestored;
-use Flarum\Post\Event\Hidden as PostHidden;
-use Flarum\Post\Event\Deleted as PostDeleted;
-use Flarum\Discussion\Event\Started;
-use Flarum\Discussion\Event\Restored as DiscussionRestored;
-use Flarum\Discussion\Event\Hidden as DiscussionHidden;
-use Flarum\Discussion\Event\Deleted as DiscussionDeleted;
-use Flarum\User\Event\Saving;
-use Flarum\Likes\Event\PostWasLiked;
-use Flarum\Likes\Event\PostWasUnliked;
 use Mattoid\MoneyHistoryAuto\Listeners\CheckinSavedHistory;
-use Mattoid\MoneyHistoryAuto\Listeners\DiscussionWasDeletedHistory;
-use Mattoid\MoneyHistoryAuto\Listeners\DiscussionWasHiddenHistory;
-use Mattoid\MoneyHistoryAuto\Listeners\DiscussionWasRestoredHistory;
-use Mattoid\MoneyHistoryAuto\Listeners\DiscussionWasStartedHistory;
-use Mattoid\MoneyHistoryAuto\Listeners\PostWasDeletedHistory;
-use Mattoid\MoneyHistoryAuto\Listeners\PostWasHiddenHistory;
-use Mattoid\MoneyHistoryAuto\Listeners\PostWasLikedHistory;
-use Mattoid\MoneyHistoryAuto\Listeners\PostWasPostedHistory;
-use Mattoid\MoneyHistoryAuto\Listeners\PostWasRestoredHistory;
-use Mattoid\MoneyHistoryAuto\Listeners\PostWasUnlikedHistory;
-use Mattoid\MoneyHistoryAuto\Listeners\UserWillBeSavedHistory;
 use Mattoid\MoneyHistoryAuto\Middleware\DistributeAllHistoryMiddleware;
 use Mattoid\MoneyHistoryAuto\Middleware\MoneyRewardsMiddleware;
 use Mattoid\MoneyHistoryAuto\Middleware\TransferHistoryMiddleware;
-use Mattoid\MoneyHistoryAuto\Middleware\UserSaveMiddleware;
 
 $extend =  [
     (new Extend\Frontend('forum'))
@@ -50,28 +27,8 @@ $extend =  [
     (new Extend\Middleware("api"))
         ->add(DistributeAllHistoryMiddleware::class)
         ->add(MoneyRewardsMiddleware::class)
-        ->add(TransferHistoryMiddleware::class)
-        ->add(UserSaveMiddleware::class),
-
-    (new Extend\Event())
-        ->listen(Posted::class, PostWasPostedHistory::class)
-        ->listen(PostRestored::class, PostWasRestoredHistory::class)
-        ->listen(PostHidden::class, PostWasHiddenHistory::class)
-        ->listen(PostDeleted::class, PostWasDeletedHistory::class)
-        ->listen(Started::class, DiscussionWasStartedHistory::class)
-        ->listen(DiscussionRestored::class, DiscussionWasRestoredHistory::class)
-        ->listen(DiscussionHidden::class, DiscussionWasHiddenHistory::class)
-        ->listen(DiscussionDeleted::class, DiscussionWasDeletedHistory::class)
-//        ->listen(Saving::class, UserWillBeSavedHistory::class)
+        ->add(TransferHistoryMiddleware::class),
 ];
-
-if (class_exists('Flarum\Likes\Event\PostWasLiked')) {
-    $extend[] =
-        (new Extend\Event())
-            ->listen(PostWasLiked::class, PostWasLikedHistory::class)
-            ->listen(PostWasUnliked::class, PostWasUnlikedHistory::class)
-    ;
-}
 
 if (class_exists('Ziven\checkin\Event\checkinUpdated')) {
     $extend[] =
